5431b1f91a9669a97249814ef8cddbb9a43229a8
aakashDeveloper <ahanda205@gmail.com>
2019-04-19 08:08:23 +0530

    a

---
+++ dashboard/create-node-app/Readme.md
@@ -0,0 +1,46 @@
+This is the seed application for node js
+
+## Available Scripts
+[![current version](https://img.shields.io/npm/v/create-node-app.svg)](https://www.npmjs.com/package/create-node-app)
+[![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
+[![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release)
+[![Greenkeeper badge](https://badges.greenkeeper.io/Aakashdeveloper/create-node-app.svg)](https://greenkeeper.io/)
+[![npm downloads](https://img.shields.io/npm/dm/create-node-app.svg?style=flat-square)](https://www.npmjs.com/package/create-node-app)
+
+## How To Start
+Inside node_modules folder
+* Navigate to @aakashdeveloper folder
+* Copy create-node-app and paste in new folder
+* Inside create-node-app run npm install > npm start
+* Navigate to http://localhost:7600
+
+
+# Available Script
+* npm start
+* npm run dev
+* npm run test
+
+## Available Test Script
+
+### `npm test`
+Using Mocha and chai for testing
+Add test cases in test folder
+
+## Database connector
+Added mongodb file with all 
+>> GET 
+>> POST 
+>> PUT 
+>> DELETE
+Operation just call mongodb file where ever required
+
+#Added Redis Sample code
+* Connect application with redis
+* Install redis server 
+* Sample code is in src/node-redis-sample.js
+
+## Added view engine layer
+* added ejs script
+
+## DeveloperLink
+> www.aakashhanda.me
---
+++ dashboard/create-node-app/src/app.js
@@ -0,0 +1,72 @@
+import express from 'express';
+import mongo from 'mongodb'
+const MongoClient = mongo.MongoClient;
+import bodyParser from 'body-parser';
+const app = express();
+const port = 7600;
+let db;
+const mongoUrl = 'mongodb://127.0.0.1:27017/'
+const col_name = 'userlist'
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+//Getting Data
+app.get('/user',(req,res) => {
+    db.collection(col_name).find().toArray((err,result) => {
+        if(err) throw err;
+        res.send(result)
+    })
+})
+
+// Adding Data
+app.post('/addUser',(req,res) => {
+    db.collection(col_name)
+        .insert(req.body,(err,result)=>{
+            if(err) throw err;
+            res.send('data inserted')  
+        })
+})
+
+// Update Data
+app.put('/update_user',(req,res) => {
+    db.collection(col_name)
+        .findOneAndUpdate({"name":req.body.name},{
+            $set:{
+                name:req.body.name,
+                email:req.body.email,
+                phone:req.body.phone
+                }
+            },
+            {
+                upsert:true
+            },(err,result) => {
+                if(err) throw err;
+                res.send(result)
+            })
+})
+
+//Delete Data
+app.delete('/deleteUser',(req,res) => {
+    db.collection(col_name).findOneAndDelete({
+        "name":req.body.name
+    },(err,result) => {
+        if(err) return res.send(500,err)
+        res.send({"message":"deleted"})
+    })
+})
+
+
+
+app.get('/',(req,res) => {
+    res.send('<h1>On home page</h1>')
+});
+
+
+MongoClient.connect(mongoUrl,(err,client) =>{
+    if(err) throw err;
+    db = client.db('apr_dashboard');
+    app.listen(port,(err)=>{
+        console.log(`server is running on port ${port}`)
+    });
+})
+
