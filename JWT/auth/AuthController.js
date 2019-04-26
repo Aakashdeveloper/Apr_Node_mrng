522097dfc7e21eca356958c2a42a1c46f1bdc3ed
aakashDeveloper <ahanda205@gmail.com>
2019-04-26 08:03:07 +0530

    a

--- JWT/auth/AuthController.js
+++ JWT/auth/AuthController.js
@@ -26,8 +26,31 @@ router.post('/register', (req,res) => {
         })
         res.status(200).send({auth:true, token: token});
     })
+})
 
-    
+router.get('/userinfo',function(req,res){
+    var token = req.headers['x-access-token'];
+    if(!token) return res.status(401).send({auth:false, message:'no token provided'})
+    jwt.verify(token, config.secret, function(err,decode){
+        if(err) return res.status(500).send({auth: false, message: 'Failed to authencticate token'})
+        User.findById(decode.id, {password:0}, function(err,user){
+            if(err) return res.status(500).send("No user found");
+            res.status(200).send(user)
+        })
+    })
+})
+
+router.post('/login',function(req,res){
+    User.findOne({email:req.body.email}, function(err,user){
+        if(err) return res.status(500).send('Error on the server');
+        if(!user) return res.status(404).send('No user found')
+        var passwordIsValid = bcrypt.compareSync(req.body.password, user.password)
+        if(!passwordIsValid) return res.status(401).send({auth:false, token:null})
+        var token = jwt.sign({id:user._id}, config.secret,{
+            expiresIn: 86400
+        })
+        res.status(200).send({auth:true, token:token})
+    })
 })
 
 router.get('/users',(req,res) => {
